5d4f4f2bbe9b353e04612238832a272c561e8bfe
Stephen Ondieki <stevehotcodes@gmail.com>
2023-10-22 13:32:30 +0300

    working on displaying one product feature

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 document.addEventListener('DOMContentLoaded', () => {
     fetchProducts();
 });
@@ -12,6 +21,19 @@ function fetchProducts() {
         console.log("error in fetching the products", error);
     });
 }
+// async function fetchOneProduct(productId:number):Promise<IProducts | null>{
+//      try {
+//             const response = await fetch(`https://fakestoreapi.com/products/${productId}`);
+//             if (!response.ok) {
+//                 throw new Error('Network response was not ok.');
+//             }
+//             const product: IProducts = await response.json();
+//             return product;
+//         } catch (error) {
+//             console.error('There was a problem fetching the product:', error);
+//             return null;
+//         }
+//  } 
 function displayProducts(products) {
     const productsContainer = document.querySelector("#products");
     if (!productsContainer) {
@@ -21,34 +43,42 @@ function displayProducts(products) {
     products.forEach((product) => {
         const productContainer = document.createElement('div');
         productContainer.className = 'product';
+        const detailsBtnId = 'details-btn-' + product.id;
         productContainer.innerHTML = `
         <img src="${product.image}" alt="${product.title}">
         <div class="product-title">${product.title}</div>
         <div class="product-price">${product.price}</div>
-        <div class="show-details-button">details</div>
+        <div class="show-details-button" id=${detailsBtnId}>details</div>
     `;
+        productsContainer.appendChild(productContainer);
         //event listener for details button
-        const detailsButton = document.querySelector(".show-details-button");
+        const detailsButton = document.querySelector("#" + detailsBtnId);
         detailsButton ? detailsButton.addEventListener("click", () => {
             displayOneProduct(product);
-        }) : " The button does not exist";
-        productsContainer.appendChild(productContainer);
-        console.log(productsContainer);
+        }) : console.log(" The button does not exist", "#" + detailsBtnId);
+        // console.log(productsContainer)
     });
 }
 function displayOneProduct(product) {
-    const productsContainer = document.querySelector("#products");
-    productsContainer ? productsContainer.innerHTML = " " : "";
-    const productView = `
-        <img src="${product.image}" alt="${product.title}">
-        <div class="product-title">${product.title}</div>
-        <div class="product-price">${product.price}</div>
+    return __awaiter(this, void 0, void 0, function* () {
+        const productsContainer = document.querySelector("#products");
+        productsContainer ? productsContainer.innerHTML = " " : "";
+        yield fetch(`https://fakestoreapi.com/products/${product.id}`)
+            .then(response => { return response.json(); })
+            .then(data => {
+            const productView = `
+        <img src="${data.image}" alt="${data.title}">
+        <div class="product-title">${data.title}</div>
+        <div class="product-price">${data.price}</div>
         <div class="go-back-button">details</div>
+        
 
     `;
-    productsContainer.innerHTML = productView;
-    // const goBackBtn=document.querySelector(".go-back-button");
-    // goBackBtn?goBackBtn.addEventListener("click",()=>{
-    //     displayProducts(products)<IProducts[]>
-    // }) :
+            productsContainer.innerHTML = productView;
+        });
+        // const goBackBtn=document.querySelector(".go-back-button");
+        // goBackBtn?goBackBtn.addEventListener("click",()=>{
+        //     displayProducts(products)<IProducts[]>
+        // }) :
+    });
 }
--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded',()=>{
     fetchProducts()
 });
 
-function fetchProducts(){
+ function fetchProducts(){
     fetch('https://fakestoreapi.com/products')
     .then(response=>{
         return response.json();
@@ -22,6 +22,24 @@ function fetchProducts(){
         console.log("error in fetching the products",error);
     })
 }
+
+// async function fetchOneProduct(productId:number):Promise<IProducts | null>{
+//      try {
+//             const response = await fetch(`https://fakestoreapi.com/products/${productId}`);
+//             if (!response.ok) {
+//                 throw new Error('Network response was not ok.');
+//             }
+//             const product: IProducts = await response.json();
+//             return product;
+//         } catch (error) {
+//             console.error('There was a problem fetching the product:', error);
+//             return null;
+//         }
+        
+
+//  } 
+
+
 
 function displayProducts(products: IProducts[]) {
     const productsContainer=document.querySelector("#products");
@@ -33,44 +51,49 @@ function displayProducts(products: IProducts[]) {
     products.forEach((product)=>{
         const productContainer=document.createElement('div');
         productContainer.className='product'
+        const detailsBtnId = 'details-btn-' + product.id
         productContainer.innerHTML=`
         <img src="${product.image}" alt="${product.title}">
         <div class="product-title">${product.title}</div>
         <div class="product-price">${product.price}</div>
-        <div class="show-details-button">details</div>
+        <div class="show-details-button" id=${detailsBtnId}>details</div>
     `;
 
-        //event listener for details button
-        const detailsButton=document.querySelector(".show-details-button") as HTMLDivElement;
-        detailsButton? detailsButton.addEventListener("click",()=>{
-            displayOneProduct(product) 
-        }) : " The button does not exist"
-
-
-
     productsContainer.appendChild(productContainer);
-    console.log(productsContainer)
+    //event listener for details button
+    const detailsButton=document.querySelector("#"+detailsBtnId) as HTMLDivElement;
+    detailsButton? detailsButton.addEventListener("click",()=>{
+        displayOneProduct(product) 
+    }) :console.log(" The button does not exist","#"+detailsBtnId)
+
+    // console.log(productsContainer)
 
     })
 
 }
 
-function displayOneProduct(product:IProducts){
+async function displayOneProduct(product:IProducts){
     const productsContainer=document.querySelector("#products") as HTMLDivElement;
+
     productsContainer?productsContainer.innerHTML=" " : ""
-    const productView=`
-        <img src="${product.image}" alt="${product.title}">
-        <div class="product-title">${product.title}</div>
-        <div class="product-price">${product.price}</div>
+  await fetch(`https://fakestoreapi.com/products/${product.id}`)
+    .then(response=>{return response.json()})
+    .then(data=>{   
+         const productView=`
+        <img src="${data.image}" alt="${data.title}">
+        <div class="product-title">${data.title}</div>
+        <div class="product-price">${data.price}</div>
         <div class="go-back-button">details</div>
+        
 
     `
     productsContainer.innerHTML=productView
+    })
+    
 
     // const goBackBtn=document.querySelector(".go-back-button");
     // goBackBtn?goBackBtn.addEventListener("click",()=>{
     //     displayProducts(products)<IProducts[]>
     // }) :
-
 
 }
